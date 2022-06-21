65ecc120a8c5cfcb9f0b8fd1702f8d54bb005113
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-06-21 15:22:09 -0700

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require 'pry'
 
+class Book
+    attr_reader :title, :turn_page
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts @turn_page = "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+require "pry"
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition, :cobble, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts @cobble = "Your shoe is as good as new!"
+             @condition = "new"
+    end
+end
+
+shoe = Shoe.new("Nike")
+shoe.color = "red"
+shoe.size = 9.5
